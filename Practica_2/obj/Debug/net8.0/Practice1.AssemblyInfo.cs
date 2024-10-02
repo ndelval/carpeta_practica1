b7c78a9b6eb49fa73f660c63776bfa8954daa620
ndelval <202210064@alu.comillas.edu>
2024-10-02 20:58:52 +0200

    Cambios finales

--- Practica_2/obj/Debug/net8.0/Practice1.AssemblyInfo.cs
+++ Practica_2/obj/Debug/net8.0/Practice1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fccbbce5334c1f9c821de6e2872839ce66ef4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659694cefb2c676b6afeeb18ba17defe838cc6ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
